a79e4821abb9abb6dcb2bededbfbc5ea2a2a2106
MarcosMorais5228 <5228.morais@gmail.com>
2025-04-22 16:39:31 -0300

    JS

--- aula14ex1/script.js
+++ aula14ex1/script.js
@@ -1,23 +1,5 @@
 botao.addEventListener('click', contagem)
-function contagem(){
-    var c = Number(document.getElementById('txtinicio').value)
-    var f = Number(document.getElementById('txtfim').value)
-    var p = Number(document.getElementById('txtpasso').value)
-    var res = document.getElementById('res')
-    
-    if (c.length == 0|| f.length == 0 || p.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
-    } else {
-        res.innerHTML = 'Contando:'
-        while(c <= f){
-            res.innerHTML += `${c} \u{1F449}`
-            c += p
-        }
-        res.innerHTML += `\u{1F3C1}`
-    }
-}
-
-/*function contagem() {
+function contagem() {
     let ini = document.getElementById('txtinicio')
     let fim = document.getElementById('txtfim')
     let passo = document.getElementById('txtpasso')
@@ -30,10 +12,15 @@ function contagem(){
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c+=p){
-            res.innerHTML  += `${c} \u{1F449}`
-        }
+        if(i<f){
+            for(let c = i; c <= f; c+=p){
+                res.innerHTML  += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c-=p){
+                res.innerHTML  += `${c} \u{1F449}`
+            }
+        }      
         res.innerHTML += `\u{1F3C1}`
     }
-}*/
+}
